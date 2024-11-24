80aed9082a2dd312b21153d08973020ba05b982f
marouane-sadoune <marouanesadoune@gmail.com>
2024-11-24 23:37:30 +0100

    update1 file twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starshop parts!');
+        $startshipCount = 456;
+
+        $myShip = [
+            'name' => 'USS Leafy(NC1180)',
+            'class' => 'Garden',
+            'caption' => 'Jordan',
+            'status' => 'underConstruction' 
+        ];
+
+        return $this->render('main/homepage.html.twig',[
+            'numberOfStarShips' => $startshipCount,
+            'myShip' => $myShip,
+        ]) ;
     }
 }
