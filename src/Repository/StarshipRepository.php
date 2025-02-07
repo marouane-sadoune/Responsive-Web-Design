07704ee8bab7afd19fc1832c827f58db8726ec7f
marouane-sadoune <marouanesadoune@gmail.com>
2025-02-07 15:35:05 +0100

    api

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -5,41 +5,46 @@ namespace App\Repository;
 
 use App\Model\Starship;
 use Psr\Log\LoggerInterface;
-
 class StarshipRepository
 {
     public function __construct(private LoggerInterface $logger)
     {
-        
     }
-
     public function findAll(): array
     {
-        $this->logger->info("Starship  collection retreved");
+        $this->logger->info('Starship collection retrieved');
         return [
             new Starship(
                 1,
-                'USS Leafy(NC1180)',
+                'USS LeafyCruiser (NCC-0001)',
                 'Garden',
-                'Jarmie-Luc Pickel',
-                'repaired'
+                'Jean-Luc Pickles',
+                'taken over by Q'
             ),
             new Starship(
                 2,
-                'USS Espresso(NC1180)',
-                'Plander',
-                'Antrova',
-                'taken over by 0'
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
             ),
             new Starship(
-                1,
-                'USS Blunder(NC1180)',
-                'Delta',
-                'Katrin Jonathan',
-                'underconstraction'
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
             ),
-            
         ];
+    }
+    public function find(int $id): ?Starship
+    {
+        foreach ($this->findAll() as $starship) {
+            if ($starship->getId() === $id) {
+                return $starship;
+            }
+        }
+        return null;
     }
 }
 ?>
